c48a220e27d5bb3a55adbc2e5029cf1bdbf978b5
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 12:31:29 +0100

    Reader 1 Test

---
+++ QRReader1.py
@@ -0,0 +1,46 @@
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+detector = cv2.QRCodeDetector()
+
+
+def sw1Pressed():
+    global sw1Press
+    sw1Press = True
+
+
+sw1Press = False
+
+print("Reading QR code using Raspberry Pi camera")
+print("Press SW1 to scan.")
+
+while True:
+    if sw1Press == True:
+        _, img = cap.read()
+        data, bbox, _ = detector.detectAndDecode(img)
+
+        if bbox is not None:
+            for i in range(len(bbox)):
+                cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
+                         0, 0), thickness=2)
+
+            cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+
+            if data:
+                sw1Press = False
+                print("Data found: " + data)
+                data = ""
+
+        cv2.imshow("code detector", img)
+
+    else:
+        cap.read()
+        cv2.destroyAllWindows()
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
