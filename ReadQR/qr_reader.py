6d7f622d10e18c91ebaadb85bf4ef932ce589fc7
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 13:33:02 +0100

    Remove Other Qr Reader and Add Flask Server

---
+++ ReadQR/qr_reader.py
@@ -0,0 +1,29 @@
+from time import sleep
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
+
+# Initialize Qr Code Variable
+codeReader = ""
+
+def readQr():
+    while True:
+        _, img = cap.read()
+
+        # detect and decode
+        data, bbox, _ = detector.detectAndDecode(img)
+        # check if there is a QRCode in the image
+        if data:
+            codeReader = data
+            print("Code Reader: ", codeReader)
+            sleep(3)
+
+        # cv2.imshow("QRCODEscanner", img)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,5 @@
+from ReadQR import qr_reader
+
+# Functions
+def readQr():
+    qr_reader.readQr()
