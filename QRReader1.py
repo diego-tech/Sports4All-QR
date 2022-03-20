1a2a580fe55ea515f5cf145770110261119e2e72
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 12:38:32 +0100

    Validation Qr Code Test Fix Line to Circle

--- QRReader1.py
+++ QRReader1.py
@@ -10,7 +10,7 @@ while True:
 
     if bbox is not None:
         for i in range(len(bbox)):
-            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
+            cv2.circle(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
                                                                                        0, 0), thickness=2)
 
         cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
