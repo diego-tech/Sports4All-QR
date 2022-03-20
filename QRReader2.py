82cc64d150639d6d873cd0190d77584101872f8e
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 12:57:13 +0100

    Fix New QRReader Test

--- QRReader2.py
+++ QRReader2.py
@@ -1,38 +1,22 @@
 import cv2
-import numpy as np
-import sys
-import time
 
-if len(sys.argv) > 1:
-    inputImage = cv2.imread(sys.argv[1])
-else:
-    inputImage = cv2.imread("qrcode-learnopencv.jpg")
+cap = cv2.VideoCapture(0)
+# initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
 
-# Display barcode and QR code location
+while True:
+    _, img = cap.read()
 
+    # detect and decode
+    data, bbox, _ = detector.detectAndDecode(img)
+    # check if there is a QRCode in the image
+    if data:
+        a = data
+        break
 
-def display(im, bbox):
-    n = len(bbox)
-    for j in range(n):
-        cv2.line(im, tuple(bbox[j][0]), tuple(
-            bbox[(j+1) % n][0]), (255, 0, 0), 3)
+    cv2.imshow("QRCODEscanner", img)
+    if cv2.waitKey(1) == ord("q"):
+        break
 
-    # Display results
-    cv2.imshow("Results", im)
-
-
-qrDecoder = cv2.QRCodeDetector()
-
-# Detect and decode the qrcode
-data, bbox, rectifiedImage = qrDecoder.detectAndDecode(inputImage)
-if len(data) > 0:
-    print("Decoded Data : {}".format(data))
-    display(inputImage, bbox)
-    rectifiedImage = np.uint8(rectifiedImage)
-    cv2.imshow("Rectified QRCode", rectifiedImage)
-else:
-    print("QR Code not detected")
-    cv2.imshow("Results", inputImage)
-
-cv2.waitKey(0)
+cap.release()
 cv2.destroyAllWindows()
