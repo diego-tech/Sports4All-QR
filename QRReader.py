ccaff18d1881ae90b20de1dc16eab10f76c8c135
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 12:49:50 +0100

    Update the first qrcode validator remove tuples

--- QRReader.py
+++ QRReader.py
@@ -21,8 +21,7 @@ while True:
         for i in range(len(bbox)):
             pt1 = tuple(bbox[i][0])
             pt2 = tuple(bbox[(i+1) % len(bbox)][0])
-            cv2.line(img, pt1, pt2, color=(255,
-                     0, 0), thickness=2)
+            cv2.line(img,color=(255, 0, 0), thickness=2)
         cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
                     1, (255, 250, 120), 2)
 
