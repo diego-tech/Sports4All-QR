ad12093d2e6f43518675e26ce2d86449c9aee38a
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 13:38:18 +0100

    Update Debug

--- server.py
+++ server.py
@@ -4,10 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def readQr():
     main.readQr()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(debug=True)
