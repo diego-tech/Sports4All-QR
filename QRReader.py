09408366458efe51fe5c7fcdb7783b3b601b33a2
Diego Muñoz Herranz <diego171200@gmail.com>
2022-03-20 12:45:41 +0100

    Update the first qrcode validator

--- QRReader.py
+++ QRReader.py
@@ -19,7 +19,7 @@ while True:
     # This is how we get that Blue Box around our Data. This will draw one, and then Write the Data along with the top (Alter the numbers here to change the colour and thickness of the text)
     if(bbox is not None):
         for i in range(len(bbox)):
-            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i+1) % len(bbox)][0]), color=(255,
+            cv2.line(img, int(bbox[i][0]), int(bbox[(i+1) % len(bbox)][0]), color=(255,
                      0, 0), thickness=2)
         cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
                     1, (255, 250, 120), 2)
